a489ba5cee5f93eb1bacfa1ae2f9b0a423db4750
NikhilSehgal123 <49030309+NikhilSehgal123@users.noreply.github.com>
2021-05-24 20:11:00 +0100

    Update coinbase_creds.py

--- coinbase_creds.py
+++ coinbase_creds.py
@@ -1,3 +1,3 @@
-api_secret = "/IdFuhqaXm3Icz2hX26pjMOcNrwJ0GbHED/ZEm4a9BF6yHoxJHzJuYVWI4eFzz0Gxjl6UjoTkkS/mI3KpOGnKw=="
-api_key = "ce5f227d2507e7f0faca949859787355"
-api_pass = "059so2bgvryp"
+api_secret = ""
+api_key = ""
+api_pass = ""
